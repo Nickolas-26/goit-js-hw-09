e55417753b65edc82a090778d24627d8b133aceb
Nick Arabadzhi <91465047+Nickolas-26@users.noreply.github.com>
2022-01-11 18:44:18 +0200

    start-task-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,34 @@
+const refs = {
+  firstDelay: document.querySelector('[name="delay"]'),
+  step: document.querySelector('[name="step"]'),
+  amount: document.querySelector('[name="amount"]'),
+  btn: document.querySelector('#btn'),
+};
+refs.btn.addEventListener('click', onClickButton);
+
+function onClickButton(e) {
+  const delay = refs.firstDelay.value;
+  // for(step= 0 , i< amount step++ ){}
+  createPromise(2, 1500)
+    .then(({ position, delay }) => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(position, delay);
+      } else {
+        reject(position, delay);
+      }
+    }, delay);
+  });
 }
+const delay = refs.step.value;
+console.log(delay);
